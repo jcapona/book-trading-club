d0d7f1b75250a99039f25af489ae195bfcea459b
Jorge Capona <jcapona@gmail.com>
2016-03-07 09:22:52 -0300

    Includes user attribute, to save username

--- api/models/Prop.js
+++ api/models/Prop.js
@@ -11,6 +11,9 @@ module.exports = {
   		user_id: {
             type: 'string',
         },
+  		user: {
+            type: 'string',
+        },
         book_id: {
         	type: 'string',
         },
