f6c1aa79ba56b5db9e25f86ecae8bd1b53efdbf5
Jorge Capona <jcapona@gmail.com>
2016-03-15 15:00:43 -0300

    Uses grid to display books

--- assets/js/display-books.js
+++ assets/js/display-books.js
@@ -23,7 +23,7 @@ function search(query)
     success: function(data)
     {
       $("#feats").addClass("hidden");
-      console.log(data);
+      //console.log(data);
       displayResults(data);
     }
   });
@@ -35,22 +35,45 @@ function search(query)
 function displayResults(data)
 {
   var html = "";
-    html += "<div class='row'>";
-    for(var i=0; i < data.length; i++)
+  html += "<div class='row'>";
+  for(var i=0; i < data.length; i++)
+    {
+    html += "<div class='col-xs-4 col-md-2'>";
+    html += "<a href="+"/book/info?bid="+data[i.toString()]['id']+" class='text-center' target='_blank'>";
+    html += "<img src="+data[i.toString()]['img']+" class='thumbnail center-block' style='max-height: 200px;'>";
+    html += "</a>";
+    html += "<h4 class='text-center'>"+data[i.toString()]['title'].slice(0,15);
+    html += data[i.toString()]['title'].length > 15? "...":"";
+    html += "</h4>";
+    html += "<p class='text-center'>"+data[i.toString()]['author']+"</p>";
+    
+    if(Object.keys(usr).length > 0)
+    {
+      if(usr.id != data[i.toString()]['owner']['id'])
       {
-      html += "<div class='col-xs-4 col-md-2'>";
-      html += "<a href="+data[i.toString()]['link']+" class='text-center' target='_blank'>";
-      html += "<img src="+data[i.toString()]['img']+" class='thumbnail center-block' style='max-height: 200px;'>";
-      html += "</a>";
-      html += "<h4 class='text-center'>"+data[i.toString()]['title'].slice(0,15);
-      html += data[i.toString()]['title'].length > 15? "...":"";
-      html += "</h4>";
-      html += "<p class='text-center'>"+data[i.toString()]['author']+"</p>";
-      html += "</div>";
-    };
+        html += "<form class='form-interaction' role='form'>";
+        html += "<input type='text' class='hidden' name='b_id' value='"+data[i.toString()]['id']+"'>";
+        html += "<input type='text' class='hidden' name='u_id_rx' value='"+data[i.toString()]['owner']['id']+"'>";
+        html += "<button class='btn btn-default' type='submit'>Message</button>";
+        html += "</form>";
+      }
+      else
+      {
+        html += "<p>Your book</p>";
+      }
+    }
+    else
+    {
+      html += "<p>You must login to message the owner</p>";
+      html += '<a href="#" type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-login">';
+      html += '<p>Log In</p></a>';
+    }
 
     html += "</div>";
+  };
 
-    $("#results").html('');
-    $("#results").append(html);
+  html += "</div>";
+
+  $("#results").html('');
+  $("#results").append(html);
 }
