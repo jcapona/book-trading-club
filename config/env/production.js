70b0bec8b9238c3d672a078f5305131d63d288a5
Jorge <jcapona@gmail.com>
2017-03-02 19:47:43 -0300

    add mlab config to production env

--- config/env/production.js
+++ config/env/production.js
@@ -11,28 +11,20 @@
  */
 
 module.exports = {
-
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
-
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
-
-  // port: 80,
-
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
-
-  // log: {
-  //   level: "silent"
-  // }
-
+    connections: {
+        mongo_local: {
+            adapter: 'sails-mongo',
+            host: 'ds033143.mlab.com',
+            port: 33143,
+            user: process.env.MONGO_USER,
+            password: process.env.MONGO_PASSWORD,
+            database: 'booktrader'
+        }
+    },
+    models: {
+        connection: 'mongolabServer'
+    },
+    log: {
+         level: "silent"
+    }
 };
